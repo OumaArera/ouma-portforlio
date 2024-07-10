4805c57184332d2600fc4e9bb4e315db312f5a7d
OumaArera <johnouma999@gmail.com>
2024-07-10 15:16:18 +0300

    fix: style buttons

--- src/App.js
+++ src/App.js
@@ -14,34 +14,34 @@ const App = () => {
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow flex flex-col items-center justify-center p-4">
-          <div className="w-full flex flex-wrap justify-center mb-6 space-y-2 sm:space-x-4">
+          <div className="w-full flex flex-wrap justify-center mb-6 space-y-2 sm:space-x-4 sm:space-y-0">
             <Link 
               to="/" 
-              className="text-white bg-gray-900 hover:bg-blue-900 px-6 py-2 rounded-full flex items-center justify-center min-h-[40px]"
+              className="text-white bg-gray-900 hover:bg-blue-900 px-6 py-2 rounded-full flex items-center justify-center h-[40px] w-[150px]"
             >
               Profile
             </Link>
             <Link 
               to="/skills" 
-              className="text-white bg-gray-900 hover:bg-blue-900 px-6 py-2 rounded-full flex items-center justify-center min-h-[40px]"
+              className="text-white bg-gray-900 hover:bg-blue-900 px-6 py-2 rounded-full flex items-center justify-center h-[40px] w-[150px]"
             >
               Skills
             </Link>
             <Link 
               to="/projects" 
-              className="text-white bg-gray-900 hover:bg-blue-900 px-6 py-2 rounded-full flex items-center justify-center min-h-[40px]"
+              className="text-white bg-gray-900 hover:bg-blue-900 px-6 py-2 rounded-full flex items-center justify-center h-[40px] w-[150px]"
             >
               Projects
             </Link>
             <Link 
               to="/services" 
-              className="text-white bg-gray-900 hover:bg-blue-900 px-6 py-2 rounded-full flex items-center justify-center min-h-[40px]"
+              className="text-white bg-gray-900 hover:bg-blue-900 px-6 py-2 rounded-full flex items-center justify-center h-[40px] w-[150px]"
             >
               Services
             </Link>
             <Link 
               to="/clients" 
-              className="text-white bg-gray-900 hover:bg-blue-900 px-6 py-2 rounded-full flex items-center justify-center min-h-[40px]"
+              className="text-white bg-gray-900 hover:bg-blue-900 px-6 py-2 rounded-full flex items-center justify-center h-[40px] w-[150px]"
             >
               Clients
             </Link>
