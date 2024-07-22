f77b7e3fc386614b1135d852ca5e232a78b5f1d7
OumaArera <johnouma999@gmail.com>
2024-07-22 14:02:17 +0300

    feat: add new client

--- src/Clients.jsx
+++ src/Clients.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import naya from "./naya.png";
+import insight from "./insight.png"
 
 const Clients = () => {
   const clientDetails = [
     {
       name: 'Eliakim Naya',
+      location: "Nairobi, Kenya",
       business: 'Motor Vehicle Dealer',
       solution: 'The app was designed to streamline vehicle inventory management and improve the customer purchase experience.',
       technologies: ['React', 'Spring Boot', 'Postgresql'],
       websiteUrl: 'https://ouma-portforlio.vercel.app/',
       imageUrl: naya,
+    },
+    {
+      name: 'Insight Wellbeing P/L',
+      location: "Harare, Zimbabwe",
+      business: 'Psychotherapy Clinic',
+      solution: 'The application enables patients seeking psychotherapy services to connect with Insight Wellbeing P/L online. It allows doctors to assign and monitor patient activities, provide prescriptions, and offer impressions that patients can download. The app ensures end-to-end encryption of all patient data and provides secure access to patient information.',
+      technologies: ['React', 'Flask', 'Postgresql'],
+      websiteUrl: 'https://insight-wellbeing.vercel.app/',
+      imageUrl: insight,
     }
-    // Add more clients as needed
+    
   ];
 
   return (
@@ -27,6 +38,7 @@ const Clients = () => {
             />
             <div className="p-4">
               <h2 className="text-xl font-semibold mb-2">{client.name}</h2>
+              <h3 className="text-md font-semibold mb-2">{client.location}</h3>
               <p className="text-gray-600 mb-2">{client.business}</p>
               <p className="text-gray-700 mb-4">{client.solution}</p>
               <div className="flex flex-wrap">
