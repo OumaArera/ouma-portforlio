7b481fd1384dc6d9fd03391dd43585ea55785c28
OumaArera <johnouma999@gmail.com>
2024-08-14 17:56:53 +0300

    feat: new feature

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -9,17 +9,17 @@ const Footer = () => {
 
   return (
     <footer className="p-6 bg-green-950 text-center text-white">
-      <div className="mb-4">
-        <nav className="mb-4">
-          <Link to="/" className="mx-2 hover:text-green-600 hover:underline">Home</Link>
-          <Link to="/services" className="mx-2 hover:text-green-600 hover:underline">Services</Link>
-          <Link to="/about" className="mx-2 hover:text-green-600 hover:underline">About</Link>
-          <Link to="/clients" className="mx-2 hover:text-green-600 hover:underline">Clients</Link>
-          <Link to="/blogs" className="mx-2 hover:text-green-600 hover:underline">Blogs</Link>
-          <Link to="/skills" className="mx-2 hover:text-green-600 hover:underline">Skills</Link>
-          <Link to="/projects" className="mx-2 hover:text-green-600 hover:underline">Projects</Link>
+      <div className="flex flex-col items-center mb-4">
+        <nav className="flex flex-wrap justify-center mb-4">
+          <Link to="/" className="mx-2 mb-2 text-sm hover:text-green-600 hover:underline">Home</Link>
+          <Link to="/services" className="mx-2 mb-2 text-sm hover:text-green-600 hover:underline">Services</Link>
+          <Link to="/about" className="mx-2 mb-2 text-sm hover:text-green-600 hover:underline">About</Link>
+          <Link to="/clients" className="mx-2 mb-2 text-sm hover:text-green-600 hover:underline">Clients</Link>
+          <Link to="/blogs" className="mx-2 mb-2 text-sm hover:text-green-600 hover:underline">Blogs</Link>
+          <Link to="/skills" className="mx-2 mb-2 text-sm hover:text-green-600 hover:underline">Skills</Link>
+          <Link to="/projects" className="mx-2 mb-2 text-sm hover:text-green-600 hover:underline">Projects</Link>
         </nav>
-        <p className="text-white hover:text-green-600 hover:underline">
+        <p className="text-white text-sm">
           © {getCurrentYear()} John Ouma. All rights reserved.
         </p>
       </div>
