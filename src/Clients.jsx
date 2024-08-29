54ef737ff3041a39aab77a503ea5032c916305ef
OumaArera <johnouma999@gmail.com>
2024-08-30 00:41:55 +0300

    feat: hide scroll bar

--- src/Clients.jsx
+++ src/Clients.jsx
@@ -26,7 +26,6 @@ const Clients = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <br />
       <h1 className="text-3xl font-bold mb-8 text-center text-green-800">Clients</h1>
       <div className="grid gap-6 lg:grid-cols-1 xl:grid-cols-1">
         {clientDetails.map((client, index) => (
