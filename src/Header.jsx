b44ea1ba88fc91341c187a202cce45f7dcac1490
OumaArera <johnouma999@gmail.com>
2025-03-06 21:30:13 +0300

    feat: Branch details

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import portrait from "./portrait.jpg";
+import portrait from "./me.png";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
